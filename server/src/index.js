37cd32d8852eb405a85f350919eb35f6b2742729
harshmall28 <contact.harshmall@gmail.com>
2026-04-30 15:11:07 +0530

    added path to cors server

--- server/src/index.js
+++ server/src/index.js
@@ -26,7 +26,17 @@ const server = new ApolloServer({
 
 await server.start();
 
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "http://127.0.0.1:5500",
+      "https://search-green-three.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
